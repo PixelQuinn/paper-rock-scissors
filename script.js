16026099e8ebda7da4daf3eba29ffe412c60553e
PixelQuinn <quinn.reams@icloud.com>
2023-05-24 11:42:29 -0400

    feat: function added to check for a winner

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Set up initial variables
-//Create Function for computer choice
+//Function for computerChoices
 function getComputerChoice() {
     var randomNum = Math.floor(Math.random() * 3) + 1;
     switch (randomNum) {
@@ -13,7 +13,26 @@ function getComputerChoice() {
 }
 
 
-//function to decide winner
+//function to decide winner, inital goal is the function to keep score, first to five wins.
+function incrementScore(score) {
+    return score + 1;
+}
+
+function checkWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "Tie";
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper') ||
+        (playerChoice === 'paper' && computerChoice === 'rock')
+    ) {
+        playerScore = incrementScore(playerScore);
+        return "player";
+    } else {
+        computerScore = incrementScore(computerScore);
+        return "computer";
+    }
+}
 //main game function
 
 
