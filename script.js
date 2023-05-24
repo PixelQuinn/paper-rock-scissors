3157900fe347c8a4cd3271be5cc8fcef0f71d87e
PixelQuinn <quinn.reams@icloud.com>
2023-05-24 10:53:56 -0400

    switch case made for random generation

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //Set up initial variables
+const computerChoice = getComputerChoice
 //Create Function for computer choice
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 1:
+            return 'paper';
+        case 2:
+            return 'rock';
+        case 3: 
+            return 'scissors';
+    }
+}
 //function to decide winner
 //main game function
 
